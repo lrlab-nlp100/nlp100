bccf70e7cd38597a4547025ea2e29a94bd44bc06
moajo <mimirosiasd@gmail.com>
2017-05-16 19:12:57 +0900

    ~p82

---
+++ moajo/chapter9/p82.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from
+
+def ext_context(sent:List[str]):
+    return None
+
+def main():
+    with open("p81_countries.txt", "r", encoding="utf-8")as countries:
+        hash_ = map(lambda line: (line[:-1], line[:-1].replace(" ", "_")), countries)  # last word is \n
+        hash_ = filter(lambda c: c[0] != c[1], hash_)
+        cs = []
+        # print(hash_)
+        for a in hash_:
+            print(a)
+            cs.append(a)
+        with open("p80_formatted.txt", "r", encoding="utf-8")as formatted:
+            with open("p81_replaced.txt", "w", encoding="utf-8") as w:
+                for line in formatted:
+                    for c in cs:
+                        line = line.replace(c[0], c[1])
+                    w.write(line)
+
+
+if __name__ == "__main__":
+    main()
