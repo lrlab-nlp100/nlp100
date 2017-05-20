ab1e035895be01acb1c7f92e2ea550b0c610cbd8
masuda <masuda.r.ac@m.titech.ac.jp>
2017-05-21 05:17:14 +0900

    chapter 03

--- masuda/nlp24.py
+++ masuda/nlp24.py
@@ -1,8 +1,8 @@
 import re
 
 with open("text_about_england.txt", encoding="utf-8") as f:
-    p = re.compile(r"(?<=\[\[Category:).*(?=\]\])")
+    p = re.compile(r"(?<=\[\[File:)[^|]*(?=|)")
     for line in f:
-        m = p.search(line) # matchは(?<=)を使えない
+        m = p.search(line)
         if m is not None:
             print(m.group(0))
