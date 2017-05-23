2f3643fafb1bee949a9b4dd97c1142f44c195777
masuda <masuda.r.ac@m.titech.ac.jp>
2017-05-23 17:24:56 +0900

    temp

---
+++ masuda/nlp30.py
@@ -0,0 +1,22 @@
+def load_mecab(mecab_file_name):
+    import re
+
+    with open(mecab_file_name, encoding="utf-8") as f:
+        morph_list = []
+        reg = re.compile(r"[\t,]")
+        for line in f:
+            if "EOS" in line:
+                continue
+            else:
+                morph = reg.split(line)
+                morph_dic = ({"surface":morph[0],
+                              "base":morph[7],
+                              "pos":morph[1],
+                              "pos1":morph[2]})
+                print(morph_dic)
+                morph_list.append(morph_dic)
+
+        return morph_list
+
+if __name__ == "__main__":
+    load_mecab("neko.txt.mecab")
