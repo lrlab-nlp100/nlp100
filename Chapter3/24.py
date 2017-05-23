411fc218d0002a9d0179958028faaad7162c317e
gooklim <gooklim1221@gmail.com>
2017-05-23 18:25:11 +0900

    kokurin

--- Chapter3/24.py
+++ Chapter3/24.py
@@ -2,9 +2,9 @@ import re
 
 with open('uk.txt','r') as f:
     for line in f:
-        target=re.findall("File:(.+?)\|",line)
+        target=re.findall("(ファイル|File):(.+?)\|",line)
         if target:
-            print(target[0])
+            print(target[0][1])
 
         # "formhash\" value=\"(.*)\""
         #print(re.findall(r'\[Category:.*\]',line))
