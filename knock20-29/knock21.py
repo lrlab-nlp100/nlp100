0d6c331ce9f0f5339859e360a2800044df319463
machida <t13304110@gunma-u.ac.jp>
2017-05-23 18:22:37 +0900

    knock20-24

---
+++ knock20-29/knock21.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import json, re
+
+with open('uk.json', 'r') as f:
+    lod = json.load(f)
+    text = lod['text'].split('\n')
+    for t in text:
+        if re.match('\[\[Category:.*\]\]', t):
+            print(t)
