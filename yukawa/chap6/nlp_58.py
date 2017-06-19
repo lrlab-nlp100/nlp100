1fac0d4552213b35261ef703870f5fd664cd759d
haraita <4getmenot0802@gmail.com>
2017-06-19 09:21:40 +0900

    chap9-82

--- yukawa/chap6/nlp_58.py
+++ yukawa/chap6/nlp_58.py
@@ -1,41 +1,12 @@
 # -*- coding: utf-8 -*-
-import re
-pa_d = re.compile(r'dep type=\"dobj\"')
-pa_s = re.compile(r'dep type=\"nsubj\"')
-pa_g = re.compile(r'<governor idx=(.+?)>(.+?)</governor>')
-pa_de = re.compile(r'<dependent idx=(.+?)>(.+?)</dependent>')
-pa_c = re.compile(r'collapsed-dependencies')
-flag,s_flag,d_flag = 0,0,0
-sub_p,dob_p,dob_c,sub_c = None,None,None,None
-sub,dob,dobc,subc = '','','',''
-with open('nlp.txt.xml','r') as f:
-    for line in f:
-        if pa_c.search(line) is not None:
-            flag += 1
-            s_flag = 0
-            d_flag = 0
-        if flag != 0 and pa_s.search(line) is not None:
-            s_flag += 1
-        if flag != 0 and pa_d.search(line) is not None:
-            d_flag += 1
-        if s_flag != 0:
-            sub_p = pa_g.search(line)
-            sub_c = pa_de.search(line)
-            if sub_p is not None:
-                sub = sub_p.group(2)
-            if sub_c is not None:
-                subc = sub_c.group(2)
-                s_flag = 0
-        if d_flag != 0:
-            dob_p = pa_g.search(line)
-            dob_c = pa_de.search(line)
-            if dob_p is not None:
-                dob = dob_p.group(2)
-            if dob_c is not None:
-                dobc = dob_c.group(2)
-                d_flag = 0
-        if sub != '' and subc != '' and dobc != '' and sub == dob: 
-            print(subc+'\t'+sub+'\t'+dobc)
-            s_flag = 0
-            dob,sub,dobc,subc = '','','',''
-            d_flag = 0
+import xml.etree.ElementTree as et
+tree = et.parse('nlp.txt.xml')
+root = tree.getroot()
+collist = root.findall(".//*[@type='collapsed-dependencies']")
+for col in collist:
+    sublist = col.findall(".//*[@type='nsubj']")
+    doblist = col.findall(".//*[@type='dobj']")
+    for sub in sublist:
+        for dob in doblist:
+            if sub[0].attrib['idx'] == dob[0].attrib['idx']:
+                print(sub[1].text+'\t'+sub[0].text+'\t'+dob[1].text)
--- yukawa/chap9/nlp_81.py
+++ yukawa/chap9/nlp_81.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 from collections import defaultdict
-with open('corpus.txt','r') as f,open('name.txt','r') as g:
+with open('corpus.txt','r+') as f, open('name.txt','r') as g:
     name = []
     for line in g:
         name.append(line[:-1])
@@ -11,5 +11,5 @@ with open('corpus.txt','r') as f,open('name.txt','r') as g:
             c_of_line = re.finditer(country,line)
             if c_of_line is not None:
                 for m in c_of_line:
-                    line = re.sub(line[m.start():m.end()],re.sub(' ','_',line[m.start():m.end()]),line)
+                    line = re.sub(line[m.start():m.end()],re.sub(u' ',u'_',line[m.start():m.end()]),line)
         f.write(line)
